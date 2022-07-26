d79669ba8d47bcc5fa61b28b1c72427e22bffc16
Dhaval Vachheta <79516768+Dhaval-Vachheta@users.noreply.github.com>
2022-07-26 12:00:08 +0530

    Update streamlit_app.py
    Add Breakfast Information.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,12 @@ import streamlit
 
 # Title
 streamlit.title('My Parents New Healthy Dinner')
+
+# BreakFast Information - START
+# BreakFast Header
+streamlit.header('BreakFast Menu')
+
+# Simple Text
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
