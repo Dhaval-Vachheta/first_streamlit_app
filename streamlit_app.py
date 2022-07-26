d27ad5da170f3d494fa1a348b8e868bd9af46c05
Dhaval Vachheta <79516768+Dhaval-Vachheta@users.noreply.github.com>
2022-07-26 12:19:46 +0530

    Update streamlit_app.py
    Fix error.

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ my_fruits_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/d
 streamlit.multiselect("Pick Some Fruits : ", list(my_fruits_list.index))
 
 # Choose the fruits name column as the Index
-my_fruits_list = my_fruits_list.set_index('Fruits)
+my_fruits_list = my_fruits_list.set_index('Fruits')
 
 # Data
 streamlit.dataframe(my_fruits_list)
